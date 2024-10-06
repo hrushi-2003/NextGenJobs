5de220e9e3496bcf9770099b94aeab6a90b53c46
Hrushikesh Reddy <hrushikesh200802@gmail.com>
2024-10-06 12:29:41 +0530

    first endpoint in index.js file is created

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,29 @@
- 
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = express();
+const PORT =3000;
+
+app.get("/home",(req,res)=>{
+  res.status(200).json({
+    message:"Hi dude you are on perfect pace keep it up",
+    success:true
+  })
+})
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+const corsOptions={
+  origin :"http//localhost:5173",
+  credentials:true
+};
+app.use(cors(corsOptions));
+
+
+
+app.listen(PORT,()=>{
+  console.log(`server running in the port ${PORT}`);
+})
