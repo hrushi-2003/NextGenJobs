31680c9b61d567849e8758bd0f6f8ae9cef14765
Hrushikesh Reddy <hrushikesh200802@gmail.com>
2024-10-07 21:32:41 +0530

    application schema created succesfully

--- backend/models/application.model.js
+++ backend/models/application.model.js
@@ -2,6 +2,18 @@ import mongoose from "mongoose";
 const applicationSchema= mongoose.Schema({
   job:{
     type:mongoose.Schema.Types.ObjectId,
-    
+    ref:'Job',
+    required:true
+  },
+  applicant:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:'User',
+    required:true
+  },
+  status:{
+    type:String,
+    enum:['pending','accepted','rejected'],
+    default:'pending'
   }
-})
+},{timestamp:true})
+export const Application=mongoose.model('Application',applicationSchema);
